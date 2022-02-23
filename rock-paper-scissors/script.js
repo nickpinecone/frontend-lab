2f15b9cb9c718811bdad0dbcfae51207f66b1339
ThreeDots <gogi.gigtar@gmail.com>
2022-02-23 20:00:33 +0500

    Well... It's a bit better

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -7,11 +7,19 @@ let output = document.querySelector("#output");
 let playerOutput = document.querySelector("#player-score");
 let computerOutput = document.querySelector("#computer-score");
 
+let playerImage = document.querySelector("img.player");
+let computerImage = document.querySelector("img.computer");
+
 let playerScore = 0;
 let computerScore = 0; 
 
 const numOfChoices = 3;
 const choices = ["rock", "paper", "scissors"];
+
+function changeImages(playerChoice, computerChoice) {
+    computerImage.src = "imgs/" + computerChoice + ".jpg";
+    playerImage.src = "imgs/" + playerChoice + ".jpg";
+}
 
 function computerPlay() {
     let randomChoice = Math.floor(Math.random() * numOfChoices)
@@ -20,6 +28,7 @@ function computerPlay() {
 
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
+    changeImages(playerSelection, computerSelection);
 
     if (playerSelection == computerSelection) {
         output.textContent = ("Tie! " + playerSelection + " vs " + computerSelection);
