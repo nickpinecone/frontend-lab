cc66503412a333b366a1dd6d686f665b985100dc
ThreeDots <gogi.gigtar@gmail.com>
2022-03-03 20:25:00 +0500

    Precedence working, prepare for refactoring

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,48 @@
 const operators = ["+", "-", "*", "/"];
 
 // parses full expression (e.g. 12 + 30 / 5 - 1)
-
-// RIGHT NOW evaluates in consequence, no precedence
-// TODO add precedence
-
 function pullExpression(expression) {
-    let isSecondOperator = false;
     let singleExpression = "";
 
+    // Check for multiply and divide operators first
+    if(expression.includes("*") || expression.includes("/")) {
+        let precedenceRegex = /[\*\/]/;
+        let operatorIndex = expression.search(precedenceRegex);
+
+        // start at operator's index, go down until meet another 
+        // operator. Thats out start index
+        let startIndex = operatorIndex-1;
+        while(!operators.includes(expression[startIndex])) {
+            startIndex -= 1;
+
+            // If in the start of expression
+            if(startIndex < 0) {
+                startIndex = -1;
+                break;
+            }
+        }
+
+        // same thing but going up
+        let endIndex = operatorIndex+1;
+        while(!operators.includes(expression[endIndex])) {
+            endIndex += 1;
+
+            // same check
+            if(endIndex > expression.length) {
+                endIndex = expression.length;
+                break;
+            }
+        }
+
+        singleExpression = expression.substring(startIndex+1, endIndex);
+        console.log(singleExpression);
+
+        return singleExpression;
+    }
+
+    let isSecondOperator = false;
+
+    // No multiplies or divides - search in continuous order
     for (let i = 0; i < expression.length; i++) {
         let element = expression[i];
 
@@ -72,6 +106,8 @@ function operate(expression) {
 }
 
 function parseExpression(expression) {
+    console.log(expression);
+
     while(true) {
         let containsOperator = expression.split("").some((value) =>
             operators.includes(value)
@@ -88,4 +124,4 @@ function parseExpression(expression) {
     }
 }
 
-parseExpression("12/2+3*2/2/3");
+parseExpression("30-15/3*5");
