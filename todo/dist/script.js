c67422e22a78ceb1f10b65b6439b9b28926d365d
ThreeDots <gogi.gigtar@gmail.com>
2022-03-10 22:17:07 +0500

    Added the ability to add and remove tasks

--- todo/dist/script.js
+++ todo/dist/script.js
@@ -1,1 +1,29 @@
 "use strict";
+const addButton = document.querySelector(".add");
+const inputTask = document.querySelector("#entered-task");
+const tasksList = document.querySelector(".tasks");
+addButton.addEventListener("click", addTask);
+inputTask.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        addTask();
+    }
+});
+function addTask() {
+    if (inputTask.value == "") {
+        return;
+    }
+    const listItem = document.createElement("li");
+    const taskName = inputTask.value;
+    inputTask.value = "";
+    const listName = document.createElement("span");
+    listName.textContent = taskName;
+    listItem.appendChild(listName);
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "x";
+    removeButton.classList.add("remove");
+    listItem.appendChild(removeButton);
+    removeButton.addEventListener("click", () => {
+        tasksList.removeChild(listItem);
+    });
+    tasksList.appendChild(listItem);
+}
---
+++ todo/src/script.ts
@@ -0,0 +1,35 @@
+const addButton: HTMLButtonElement = document.querySelector(".add")!;
+const inputTask: HTMLInputElement = document.querySelector("#entered-task")!;
+const tasksList: HTMLUListElement = document.querySelector(".tasks")!;
+
+addButton.addEventListener("click", addTask);
+inputTask.addEventListener("keydown", (event) => {
+    if(event.key == "Enter") {
+        addTask();
+    }
+});
+
+function addTask() {
+    if(inputTask.value == "") {
+        return;
+    }
+
+    const listItem = document.createElement("li");
+
+    const taskName = inputTask.value;
+    inputTask.value = "";
+    const listName = document.createElement("span");
+    listName.textContent = taskName;
+    listItem.appendChild(listName);
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "x";
+    removeButton.classList.add("remove");
+    listItem.appendChild(removeButton);
+
+    removeButton.addEventListener("click", () => {
+        tasksList.removeChild(listItem);
+    });
+
+    tasksList.appendChild(listItem);
+}
