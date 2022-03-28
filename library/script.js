044258f00c17d97c48076ee339df1862f3133068
ThreeDots <gogi.gigtar@gmail.com>
2022-03-28 16:35:20 +0500

    Styling done

---
+++ library/script.js
@@ -0,0 +1,14 @@
+// const button = document.querySelector(".status");
+
+// button.addEventListener("click", () => {
+//     if(button.classList.contains("not-read")) {
+//         button.classList.remove("not-read");
+//         button.classList.add("read");
+//     }
+//     else {
+//         button.classList.remove("read");
+//         button.classList.add("not-read");
+//     }
+// });
+
+const addButton = document.querySelector(".add");
