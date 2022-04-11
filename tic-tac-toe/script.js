0b0a1e32d3291b6a29ba73b324f3599587be0dc8
ThreeDots <gogi.gigtar@gmail.com>
2022-04-11 18:51:26 +0500

    Basic skeleton for gameplay

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -14,6 +14,7 @@ let display = (function() {
     let pvpButton = document.querySelector(".pvp > .mode");
     let pveButton = document.querySelector(".pve > .mode");
     let pveSymbols = document.querySelectorAll(".pve > .choice > button");
+    let pvpSymbols = document.querySelectorAll(".pvp > .choice > button");
 
      function changeMode(type) {
         changeBottomBar(type);
@@ -57,6 +58,17 @@ let display = (function() {
         newSymbol.classList.add("chosen");
     }
     
+    function highlightSymbol(symbol) {
+        let currentMode = (state.getMode() == Mode.PVE) ? pveSymbols : pvpSymbols;
+
+        currentMode.forEach((el) => {
+            if(el.textContent == symbol){
+                removeCurrentChosen();
+                el.classList.add("chosen");
+            }
+        });
+    }
+
     function chooseSymbol(event) {
         removeCurrentChosen();
         state.changeSymbol(Symbol.O);
@@ -81,7 +93,53 @@ let display = (function() {
         currentSymbol.classList.remove("chosen");
     }
 
-    return {changeMode, disableChoosing};
+    function updateFirstScore(score) {
+        let split = player1.textContent.split(" ");
+        player1.textContent = split[0] + " " + score.toString();
+    }
+
+    function updateSecondScore(score) {
+        let split = player2.textContent.split(" ");
+        player2.textContent = split[0] + " " + score.toString();
+    }
+
+    function resetScore() {
+        updateFirstScore(0);
+        updateSecondScore(0);
+    }
+
+    return {changeMode, disableChoosing, resetScore, updateFirstScore, updateSecondScore, highlightSymbol};
+})();
+
+let board = (function() {
+    let container = document.querySelector(".board");
+    let cells = document.querySelectorAll(".cell");
+
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+
+    // Caculate the 2d array spot
+    // get number attribute
+    // let row = number // 3
+    // let column = number % 3
+
+    function hoverAnimation() {
+        cells.forEach((cell) => {
+            cell.addEventListener("click", () => {
+                cell.textContent = state.getSymbol();
+                cell.style.color = "black";
+                cell.removeEventListener("mouseleave", () => {});
+                display.disableChoosing();
+
+                state.nextTurn();
+            });
+        });
+    }
+
+    hoverAnimation();
 })();
 
 let state = (function() {
@@ -90,20 +148,54 @@ let state = (function() {
 
     let type = Mode.PVP;
     let symbol = Symbol.X;
+    let score1 = 0;
+    let score2 = 0;
 
     function bindEvents() {
         pvpButton.addEventListener("click", () => {
             if(type != Mode.PVP) {
                 changeMode(Mode.PVP);
                 symbol = Symbol.X;
+                resetScore();
             }
         });
         pveButton.addEventListener("click", () => {
             if(type != Mode.PVE) {
                 changeMode(Mode.PVE);
                 symbol = Symbol.X;
+                resetScore();
             }
         });
+    }
+
+    function increaseFirstScore() {
+        score1 += 1;
+        display.updateFirstScore(score1);
+    }
+
+    function increaseSecondScore() {
+        score2 += 1;
+        display.updateSecondScore(score2);
+    }
+
+    function resetScore() {
+        score1 = 0;
+        score2 = 0;
+        display.resetScore();
+
+    }
+
+    function checkForWin() {
+        return false;
+    }
+
+    function nextTurn() {
+        if(checkForWin()) {
+
+        }
+        
+        symbol = (symbol == Symbol.X) ? Symbol.O : Symbol.X;
+        display.highlightSymbol(symbol);
     }
 
     function changeMode(newType) {
@@ -125,42 +217,5 @@ let state = (function() {
 
     bindEvents();
 
-    return {getMode, changeMode, getSymbol, changeSymbol};
-})();
-
-let board = (function() {
-    let container = document.querySelector(".board");
-    let cells = document.querySelectorAll(".cell");
-
-    let board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ]
-
-    // Caculate the 2d array spot
-    // get number attribute
-    // let row = number // 3
-    // let column = number % 3
-
-    function hoverAnimation() {
-        cells.forEach((cell) => {
-            cell.addEventListener("mouseenter", () => {
-                cell.textContent = state.getSymbol();
-                cell.style.color = "gray";
-            });
-            cell.addEventListener("mouseleave", () => {
-                cell.textContent = "";
-                cell.style.color = "black";
-            });
-            cell.addEventListener("click", () => {
-                cell.textContent = state.getSymbol();
-                cell.style.color = "black";
-                cell.removeEventListener("mouseleave", () => {});
-                display.disableChoosing();
-            });
-        });
-    }
-
-    hoverAnimation();
+    return {getMode, changeMode, getSymbol, changeSymbol, nextTurn};
 })();
