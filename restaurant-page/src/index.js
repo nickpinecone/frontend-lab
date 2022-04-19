38280f36e6ce804fe23ac6ea9a08e7938e12d5b8
ThreeDots <threedotsellipsis@protonmail.com>
2022-04-19 18:51:50 +0500

    Split pages to three files

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,65 +1,38 @@
-import "./style.css";
-import Logo from "./restaurant.png";
+import showHomePage from "./home";
 
 const content = document.querySelector("#content");
 
-const title = document.createElement("div");
-title.classList.add("title");
-{
-    const heading = document.createElement("h1");
-    heading.textContent = "At Marcel's";
-    title.appendChild(heading);
-}
-content.appendChild(title);
-
-const description = document.createElement("div");
-description.classList.add("description");
-{
-    const logo = document.createElement("img");
-    logo.src = Logo;
-    description.appendChild(logo);
-
-    const heading = document.createElement("h3");
-    heading.textContent = "Best Pies In The World";
-    description.appendChild(heading);
-
-    const container = document.createElement("div");
+function addBottomBar() {
+    const tabs = document.createElement("div");
+    tabs.classList.add("tabs");
     {
-        const heading = document.createElement("h4");
-        heading.textContent = "We Care About Our Customers";
-        container.appendChild(heading);
+        const homeButton = document.createElement("button");
+        homeButton.textContent = "Home";
+        homeButton.addEventListener("click", showHomePage);
+        tabs.appendChild(homeButton);
 
-        const list = document.createElement("ul");
-        let li = document.createElement("li");
-        li.textContent = "Best Quality";
-        list.appendChild(li);
-        
-        li = document.createElement("li");
-        li.textContent = "Fair Prices";
-        list.appendChild(li);
+        const foodButton = document.createElement("button");
+        foodButton.textContent = "Pies";
+        tabs.appendChild(foodButton);
 
-        li = document.createElement("li");
-        li.textContent = "Huge Selection";
-        list.appendChild(li);
+        const contactButton = document.createElement("button");
+        contactButton.textContent = "Contact";
+        tabs.appendChild(contactButton);
 
-        li = document.createElement("li");
-        li.textContent = "Blazingly Fast Delivery";
-        list.appendChild(li);
-        
-
-        container.appendChild(list);
     }
-    description.appendChild(container);
-}
-content.appendChild(description);
+    content.appendChild(tabs);
 
-const attribution = document.createElement("div");
-attribution.classList.add("attribution");
-{
-    const a = document.createElement("a");
-    a.href = "https://www.flaticon.com/free-icons/restaurant";
-    a.title = "restaurant icons";
-    a.textContent = "Restaurant icons created by Freepik";
-    attribution.appendChild(a);
+    const attribution = document.createElement("div");
+    attribution.classList.add("attribution");
+    {
+        const a = document.createElement("a");
+        a.href = "https://www.flaticon.com/free-icons/restaurant";
+        a.title = "restaurant icons";
+        a.textContent = "Restaurant icons created by Freepik";
+        attribution.appendChild(a);
+    }
+    content.appendChild(attribution);
 }
-content.appendChild(attribution);
+
+showHomePage();
+addBottomBar();
