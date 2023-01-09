8ac023357709b9dd8c80ec43d1a78c8037b2f90d
threedots <threedotsellipsis@protonmail.com>
2023-01-09 17:10:14 +0500

    Implement play round function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -6,6 +6,36 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
-for (let i = 0; i < 5; i++) {
-    console.log(getComputerChoice());
+function playRound(playerSelection, computerChoice) {
+    if (playerSelection == computerChoice) {
+        return "draw";
+    }
+    else if (
+        (playerSelection == "rock" && computerChoice == "scissors") ||
+        (playerSelection == "paper" && computerChoice == "rock") ||
+        (playerSelection == "scissors" && computerChoice == "paper")
+    ) {
+        return "win"
+    }
+    else {
+        return "lose"
+    }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = "";
+
+        while (!choices.find((value, index, obj) => value == playerSelection)) {
+            playerSelection = prompt("Choose your weapon (rock, paper, scissors): ");
+            continue
+        }
+
+        let computerChoice = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerChoice));
+    }
+}
+
+game()
+
