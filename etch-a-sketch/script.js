56d965efb18c03fddbdc40b3f32c068a86c316eb
threedots <threedotsellipsis@protonmail.com>
2023-01-21 18:40:27 +0500

    Add change grid size functionality

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,28 +1,45 @@
 let canvas = document.querySelector(".canvas");
+let changeButton = document.querySelector(".change");
 
-let mouseDown = false
+let mouseDown = false;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
+createGrid(16);
+bindEvents();
 
-        canvas.appendChild(cell);
+function createGrid(size) {
+    canvas.replaceChildren();
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            cell.addEventListener("mousedown", () => {
+                cell.style.backgroundColor = "black";
+            });
+            cell.addEventListener("mouseover", () => {
+                if (mouseDown) {
+                    cell.style.backgroundColor = "black";
+                }
+            });
+
+            canvas.appendChild(cell);
+        }
     }
+
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
-canvas.addEventListener("mousedown", () => mouseDown = true)
-window.addEventListener("mouseup", () => mouseDown = false)
+function bindEvents() {
+    canvas.addEventListener("mousedown", () => mouseDown = true);
+    window.addEventListener("mouseup", () => mouseDown = false);
 
-canvas.childNodes.forEach((cell) => {
-    cell.addEventListener("mousedown", () => {
-        cell.style.backgroundColor = "black";
+    changeButton.addEventListener("click", () => {
+        let size = prompt("Enter new canvas size: ");
+        createGrid(size);
     });
-    cell.addEventListener("mouseover", () => {
-        if (mouseDown) {
-            cell.style.backgroundColor = "black";
-        }
-    });
-});
+}
+
 
 
