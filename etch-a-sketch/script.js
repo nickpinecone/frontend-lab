65151735d20a26733c401119e1b597e8a7498102
threedots <threedotsellipsis@protonmail.com>
2023-01-21 18:17:20 +0500

    Basic drawing ability

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,10 +1,28 @@
 let canvas = document.querySelector(".canvas");
 
+let mouseDown = false
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div")
-        cell.classList.add("cell")
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
 
-        canvas.appendChild(cell)
+        canvas.appendChild(cell);
     }
 }
+
+canvas.addEventListener("mousedown", () => mouseDown = true)
+window.addEventListener("mouseup", () => mouseDown = false)
+
+canvas.childNodes.forEach((cell) => {
+    cell.addEventListener("mousedown", () => {
+        cell.style.backgroundColor = "black";
+    });
+    cell.addEventListener("mouseover", () => {
+        if (mouseDown) {
+            cell.style.backgroundColor = "black";
+        }
+    });
+});
+
+
