c00d360fe91f80dc327c04106eeffa2267e36526
threedots <threedotsellipsis@protonmail.com>
2023-01-31 19:04:45 +0500

    Add ability to chain calculatinos

--- calculator/script.js
+++ calculator/script.js
@@ -4,45 +4,66 @@ let operatorNode = document.querySelector(".operator");
 
 let buttons = document.querySelectorAll(".buttons > button");
 
+function calculate(num1, operator, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    let result = 0;
+
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        case "%":
+            result = num1 % num2;
+            break;
+    }
+
+    return +result.toFixed(2);
+}
+
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
-        if (button.classList.contains("operator") && operatorNode.textContent == "" && numberOne.textContent != "") {
-            operatorNode.textContent = button.textContent;
-        }
-
-        if (button.classList.contains("number") && operatorNode.textContent == "") {
-            numberOne.textContent += button.textContent;
-        }
-        else if (button.classList.contains("number")) {
-            numberTwo.textContent += button.textContent;
-        }
-
-        if (button.classList.contains("equals") && numberOne.textContent && operatorNode.textContent && numberTwo.textContent) {
-            let num1 = Number(numberOne.textContent);
-            let num2 = Number(numberTwo.textContent);
-            let result = 0;
-
-            switch (operatorNode.textContent) {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    break;
-                case "%":
-                    result = num1 % num2;
-                    break;
+        if (button.classList.contains("operator")) {
+            if (operatorNode.textContent == "" && numberOne.textContent != "") {
+                operatorNode.textContent = button.textContent;
             }
+            else if (operatorNode.textContent != "" && numberTwo.textContent != "") {
+                let result = calculate(numberOne.textContent, operatorNode.textContent, numberTwo.textContent);
 
-            numberOne.textContent = +result.toFixed(2);
-            numberTwo.textContent = "";
-            operatorNode.textContent = "";
+                numberOne.textContent = result;
+                numberTwo.textContent = "";
+                operatorNode.textContent = button.textContent;
+
+            }
+        }
+
+        else if (button.classList.contains("number")) {
+            if (operatorNode.textContent == "") {
+                numberOne.textContent += button.textContent;
+            }
+            else {
+                numberTwo.textContent += button.textContent;
+            }
+        }
+
+        else if (button.classList.contains("equals")) {
+            if (numberOne.textContent && operatorNode.textContent && numberTwo.textContent) {
+                let result = calculate(numberOne.textContent, operatorNode.textContent, numberTwo.textContent);
+
+                numberOne.textContent = result;
+                numberTwo.textContent = "";
+                operatorNode.textContent = "";
+            }
         }
     });
 });
