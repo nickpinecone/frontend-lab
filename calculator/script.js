8ca2bf1e56f8745128caca93789600e6f36e4bf3
threedots <threedotsellipsis@protonmail.com>
2023-02-05 14:45:53 +0500

    Minor fixes

--- calculator/script.js
+++ calculator/script.js
@@ -25,7 +25,8 @@ function calculate(num1, operator, num2) {
                 result = num1 / num2;
             break;
         case "%":
-            result = num1 % num2;
+            if (num2 != 0)
+                result = num1 % num2;
             break;
     }
 
