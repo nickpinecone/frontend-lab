0a2306e900cf7673e7e95d7d09bdad968ae86a13
threedots <threedotsellipsis@protonmail.com>
2023-08-10 17:47:06 +0500

    Change wepback config a bit

--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -1,11 +1,37 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    devtool: "inline-source-maps",
+    devtool: "inline-source-map",
     mode: "development",
-    entry: "./src/index.js",
+
+    entry: {
+        main: "./src/index.js",
+    },
     output: {
-        filename: "main.js",
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Restaurant Page",
+            template: "./src/template.html",
+            filename: "index.html",
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
