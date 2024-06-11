b57f31dc564dce2e3bd7c655adc30fa309a93431
threedots <threedotsellipsis@protonmail.com>
2024-06-11 21:15:48 +0500

    Structure modification

--- weather-app/src/script.js
+++ weather-app/src/script.js
@@ -1,1 +1,13 @@
 import "./styles.css"
+
+let conditionIcons = {
+    "thunderstorm": "🌩️",
+    "drizzle": "🌧️",
+    "rain": "🌧️",
+    "snow": "❄️",
+    "atmoshpere": "🌫️",
+    "clear": "☀️",
+    "clouds": "☁️",
+};
+
+console.log(conditionIcons["rain"]);
