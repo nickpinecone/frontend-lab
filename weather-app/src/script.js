3d308dcd7f0c615908b12887353e4cff741e32ac
threedots <threedotsellipsis@protonmail.com>
2024-06-12 22:09:51 +0500

    Change to weather api, conditions

--- weather-app/src/script.js
+++ weather-app/src/script.js
@@ -1,13 +1,40 @@
 import "./styles.css"
 
-let conditionIcons = {
-    "thunderstorm": "🌩️",
-    "drizzle": "🌧️",
-    "rain": "🌧️",
-    "snow": "❄️",
-    "atmoshpere": "🌫️",
-    "clear": "☀️",
-    "clouds": "☁️",
-};
+function containsAny(text, arr) {
+    let has = false;
 
-console.log(conditionIcons["rain"]);
+    for (let i = 0; i < arr.length; i++) {
+        const str = arr[i];
+        if (text.includes(str)) {
+            has = true;
+            break;
+        }
+    }
+
+    return has;
+}
+
+function getCondition(text) {
+    text = text.toLowerCase();
+
+    if (containsAny(text, ["cloud", "overcast"])) {
+        return { icon: "☁️️", desc: "Clouds" };
+    }
+    else if (containsAny(text, ["rain", "drizzle", "sleet"])) {
+        return { icon: "🌧️", desc: "Rain" };
+    }
+    else if (containsAny(text, ["mist", "fog"])) {
+        return { icon: "🌫️", desc: "Mist" };
+    }
+    else if (containsAny(text, ["snow", "ice", "blizzard"])) {
+        return { icon: "❄️", desc: "Snow" };
+    }
+    else if (containsAny(text, ["thunder"])) {
+        return { icon: "🌩️", desc: "Thunder" };
+    }
+    else {
+        return { icon: "☀️", desc: "Clear" };
+    }
+}
+
+console.log(getCondition("Heavy rain showers"));
